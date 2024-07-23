cbff3a54742e99c9e42e6cf0c9f2ed9167af1858
Duncan Thow <duncan.thow@students.codeinstitute.net>
2024-07-23 14:44:00 +0000

    Before Debugging

--- blog/models.py
+++ blog/models.py
@@ -13,12 +13,30 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(max_length=200, blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ["-created_on", "author"]
+    
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
+    
+
 
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments_author")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on", "author"]
+
+    def __str__(self):
+        return f"Comment: {self.body} by {self.author}"
+
+    
+
+    
     

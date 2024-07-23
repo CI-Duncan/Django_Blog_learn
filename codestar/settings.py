cc2ea1c855c72db4eedb81d27c5e13360b72b5e3
Duncan Thow <duncan.thow@students.codeinstitute.net>
2024-07-23 16:58:36 +0000

    First views created

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-ciduncan-djangobloglear-0w0e4rkbk89.ws.codeinstitute-ide.net', '.herokuapp.com']
 

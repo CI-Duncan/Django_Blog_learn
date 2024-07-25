ef2b2e47132ff06409df8c37d5196a057a5a044d
Duncan Thow <duncan.thow@students.codeinstitute.net>
2024-07-25 20:16:37 +0000

    Pre POST FORM handlers

--- blog/views.py
+++ blog/views.py
@@ -28,7 +28,7 @@ def post_detail(request, slug):
     """
 
     queryset = Post.objects.filter(status=1)
-    post = get_object_or_404(queryset, slug=slug)
+    post = get_object_or_404(queryset, slug=slug) 
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
     if request.method == "POST":
@@ -45,6 +45,7 @@ def post_detail(request, slug):
 
     comment_form = CommentForm()
 
+    
     return render(
         request,
         "blog/post_detail.html",
